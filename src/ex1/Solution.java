4e89a5eb43e9853657c79dab582ee08f887aa161
김현진 <gus10010@naver.com>
2023-01-29 16:49:28 +0900

    parseInt NumberFormatException 에러 수정

--- src/ex1/Solution.java
+++ src/ex1/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 	
     public int solution(String t, String p) {
     	int answer = 0;
-        List<Integer> answerList = new ArrayList<Integer>();
+        List<String> answerList = new ArrayList<>();
         String stT = t;
         String stP = p;
         
@@ -15,16 +15,16 @@ public class Solution {
         int iPLen = stP.length();
         
         // t 문자열 파악
-        List<Integer> arrStT = new ArrayList<>();
+        List<String> arrStT = new ArrayList<>();
         String subT = "";
         for (int idx = 0; idx + iPLen <= stT.length(); idx++) {
         	subT = stT.substring(idx, idx + iPLen);
-        	arrStT.add(Integer.parseInt(subT));
+        	arrStT.add(subT);
         }
         
         // 문자 비교
-        for (int idx : arrStT) {
-        	if (idx <= Integer.parseInt(stP)) {
+        for (String idx : arrStT) {
+        	if (idx.compareTo(stP) <= 0) {
         		answerList.add(idx);
         	}
         }
@@ -35,8 +35,8 @@ public class Solution {
     }
     
     public static void main(String[] args) {
-		String t = "3141592";
-		String p = "271";
+		String t = "311234234134132413241324";
+		String p = "24123412341234123";
 		
 		Solution solution = new Solution();
 		System.out.println(solution.solution(t, p));
